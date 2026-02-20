60bd286dc8f4110606f16d6bd4b0a889caad639a
lakshya <lakshyaparik99@gmail.com>
2026-02-20 09:53:04 +0530

    left shift right shift

---
+++ v2/leftshift-rightshift/1.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+  cout << "12<<2=" << (12 << 2) << endl; // formula n*2(number of shifts)
+
+  cout << "12>>2=" << (12 >> 2) << endl; // formula n/2(number of shifts)
+}
