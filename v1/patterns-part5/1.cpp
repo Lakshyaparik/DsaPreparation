5ff650907730b513d7cf5a6324c93f6715ada4d4
lakshya <lakshyaparik99@gmail.com>
2026-02-20 09:50:09 +0530

    patterns part 5-pyramid intro

---
+++ v1/patterns-part5/1.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+  cout << " Enter the value of n: " << endl;
+  int n;
+  cin >> n;
+  int i = 1;
+  while (i <= n)
+  {
+    int space = n - i;
+    // 1st triangle
+    while (space)
+    {
+      cout << " ";
+      space--;
+    }
+    int j = 1;
+    // 2nd triangle
+    while (j <= i)
+    {
+      cout << j;
+      j++;
+    }
+    // 3rd triangle
+    int k = i - 1;
+    while (k)
+    {
+      cout << k;
+      k--;
+    }
+    cout << endl;
+    i++;
+  }
+}
